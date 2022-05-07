dac74c2ab4a1c3f7823553ef5cdd17f34b85f909
Hiram <Hiramavilaa@gmail.com>
2022-05-07 10:36:10 -0500

    cmbio de titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div >
         
-        <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimiento pacientes<span className='text-indigo-600 font-bold'> Veterinaria</span></h1>
+        <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimiento de pacientes<span className='text-indigo-600 font-bold'> Veterinaria</span></h1>
 
 
 
